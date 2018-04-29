af0338c3f45c4965db00a4ff2c8b079e847741de
garmu <lys.jordan@gmail.com>
2018-04-29 14:44:22 -0600

    set up skeleton title-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TitleBarModule } from './title-bar/title-bar.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TitleBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
